51f7fff7a19e08cd8912b9528b9f1fe59c6d0569
skrilaxcz <skrilax@gmail.com>
2013-02-11 22:54:25 +0100

    Fixed mess in idc / keys

--- device_asanti_c.mk
+++ device_asanti_c.mk
@@ -132,12 +132,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # IDC
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/idc/melfas-ts.idc:system/usr/idc/melfas-ts.idc \
+    $(LOCAL_PATH)/idc/atmxt-i2c.idc:system/usr/idc/atmxt-i2c.idc \
     $(LOCAL_PATH)/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
     $(LOCAL_PATH)/idc/qwerty.idc:system/usr/idc/qwerty.idc \
-    $(LOCAL_PATH)/idc/evfwd.idc:system/usr/idc/evfwd.idc
+    $(LOCAL_PATH)/idc/keypad_8960.idc:system/usr/idc/keypad_8960.idc
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+# keylayouts
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/keylayout/keypad_8960.kl:system/usr/keylayout/keypad_8960.kl \
+
+# Keychars
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keychars/keypad_8960.kcm:system/usr/keychars/keypad_8960.kcm \
 
 #telephony
 #note: Sim card models do not have correct band for LTE, so turn it off
---
+++ idc/keypad_8960.idc
@@ -0,0 +1,11 @@
+
+device.internal = 1
+
+touch.deviceType = touchScreen
+touch.orientationAware = 1
+
+keyboard.layout = keypad_8960
+keyboard.characterMap = keypad_8960
+keyboard.orientationAware = 1
+keyboard.builtIn = 1
+
