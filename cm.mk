8a779fae84cf8cda8e5d37766f59caaa76c94672
nadlabak <pavel@doshaska.net>
2013-04-24 01:48:08 +0200

    Move msm8960-common/cm.mk info to devices
    Change-Id: I0cfc6e0794a33a645d10ddbc66191defa3c2e483

--- cm.mk
+++ cm.mk
@@ -1,7 +1,11 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-$(call inherit-product, device/motorola/msm8960-common/cm.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 540
