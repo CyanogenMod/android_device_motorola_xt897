f3e5474e74f8a48898b82fa86e4649af55f079ee
dhacker29 <davidhackerdvm@gmail.com>
2013-04-26 14:15:23 -0500

    Localize to device to prevent build breakage on other devices

--- Android.mk
+++ Android.mk
@@ -1,7 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(BOARD_VENDOR),motorola-msm8960)
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
+ifeq ($(TARGET_DEVICE),xt897)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
 endif
