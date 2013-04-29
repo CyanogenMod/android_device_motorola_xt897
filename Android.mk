6a95e79a0b35b842a46f995290799a341f80cad5
nadlabak <pavel@doshaska.net>
2013-04-29 10:32:56 +0200

    Open the tree also for xt897c target
    Change-Id: Ifb136ee31f283da497140ef9521c482668716227

--- Android.mk
+++ Android.mk
@@ -3,3 +3,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),xt897)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
+
+ifeq ($(TARGET_DEVICE),xt897c)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+endif
