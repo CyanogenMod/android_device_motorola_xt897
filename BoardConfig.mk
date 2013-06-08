68fb8c2fbd090903142f53b801cd56f02e7761f6
dhacker29 <davidhackerdvm@gmail.com>
2013-06-07 19:22:21 -0500

    Move common kernel build defines to common device

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,21 +13,10 @@ TARGET_USES_MOTOROLA_MSM8960_XT897_LIBLIGHT := true
 BOARD_HAVE_NFC := true
 
 # Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8960-common
-TARGET_KERNEL_CONFIG := msm8960_mmi_defconfig
 TARGET_KERNEL_VARIANT_CONFIG := msm8960_mmi_gsm_defconfig
-
-# copy all kernel modules under the "kernel/modules" directory to system/lib/modules
-PRODUCT_COPY_FILES += $(shell \
-    find $(LOCAL_PATH)/kernel/modules -name '*.ko' 2> /dev/null \
-    | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
-    | tr '\n' ' ')
-
-BOARD_KERNEL_CMDLINE := console=/dev/null androidboot.hardware=qcom user_debug=31 loglevel=1 msm_rtb.filter=0x3F kgsl.mmutype=gpummu zcache
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01600000
 
 #bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(LOCAL_PATH)/bluetooth
 
+#TWRP
+DEVICE_RESOLUTION := 540x960
