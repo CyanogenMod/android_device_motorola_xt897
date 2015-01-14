bfd9982ea00913317493fd26b99b216626fc3231
nadlabak <pavel@doshaska.net>
2015-01-14 15:27:08 +0100

    Fix inheritance
    Change-Id: Idd95f151a9c4afba11d45c5a06fab8f458f75dfb

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-$(call inherit-product, device/motorola/msm8960-common/msm8960.mk)
+$(call inherit-product, device/motorola/msm8960_jbbl-common/msm8960.mk)
 
 LOCAL_PATH := device/motorola/xt897
 
